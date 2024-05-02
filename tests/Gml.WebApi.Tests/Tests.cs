91bd23801a3cccbc333195f5cca2f286d6648e60
Anton Terentev <gamervii@icloud.com>
2024-05-02 11:08:51 +0300

    Expanded unit tests coverage in Tests.cs
    Added new unit tests in Tests.cs covering functionalities such as updating profile, getting and updating platform settings, handling texture URLs, updating sentry DSN, and updating launcher versions. Refactored existing tests for improved readability and maintainability.

--- tests/Gml.WebApi.Tests/Tests.cs
+++ tests/Gml.WebApi.Tests/Tests.cs
@@ -1,9 +1,12 @@
 using System.Net;
 using Faker;
+using Gml.Web.Api.Domains.LauncherDto;
 using Gml.Web.Api.Domains.System;
+using Gml.Web.Api.Dto.Launcher;
 using Gml.Web.Api.Dto.Messages;
 using Gml.Web.Api.Dto.Profile;
-using GmlCore.Interfaces;
+using Gml.Web.Api.Dto.Settings;
+using Gml.Web.Api.Dto.Texture;
 using GmlCore.Interfaces.Enums;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
@@ -12,10 +15,11 @@ namespace Gml.WebApi.Tests;
 
 public class Tests
 {
-    private IGmlManager _gmlManager;
     private HttpClient _httpClient;
-    private string _profileName;
+    private readonly string _profileName = Name.First();
     private WebApplicationFactory<Program> _webApplicationFactory;
+    private readonly string _newTextureUrl = "https://test.ru";
+    private readonly string _newSenryUrl = "https://sentry.test.ru";
 
     [SetUp]
     public void Setup()
@@ -54,8 +58,6 @@ public class Tests
     [Order(2)]
     public async Task CreateProfile()
     {
-        _profileName = Name.First();
-
         var profile = new MultipartFormDataContent();
         profile.Add(new StringContent(_profileName), "Name");
         profile.Add(new StringContent(Address.StreetAddress()), "Description");
@@ -91,7 +93,7 @@ public class Tests
             IsFullScreen = true,
             OsType = ((int)OsType.Windows).ToString(),
             OsArchitecture = Environment.Is64BitOperatingSystem ? "64" : "32",
-            UserAccessToken = "sergsecgrfsecgriseuhcygrshecngrysicugrbn7csewgrfcsercgser",
+            UserAccessToken = "accessToken",
             UserName = "GamerVII",
             UserUuid = "userUuid"
         });
@@ -112,6 +114,30 @@ public class Tests
 
     [Test]
     [Order(4)]
+    public async Task UpdateProfile()
+    {
+        var profileUpdateData = new MultipartFormDataContent
+        {
+            { new StringContent(_profileName), "Name" },
+            { new StringContent(_profileName), "OriginalName" },
+            { new StringContent("Avon"), "Description" },
+            { new StringContent("image"), "IconBase64" }
+        };
+
+        var response = await _httpClient.PutAsync("/api/v1/profiles", profileUpdateData);
+
+        string content = await response.Content.ReadAsStringAsync();
+        var model = JsonConvert.DeserializeObject<ResponseMessage<SettingsReadDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        });
+    }
+
+    [Test]
+    [Order(5)]
     public async Task CompileProfile()
     {
         var profile = TestHelper.CreateJsonObject(new ProfileCompileDto
@@ -128,6 +154,205 @@ public class Tests
         {
             Assert.That(model, Is.Not.Null);
             Assert.IsTrue(response.IsSuccessStatusCode);
+        });
+    }
+
+    [Test]
+    [Order(6)]
+    public async Task GetSettings()
+    {
+
+        var response = await _httpClient.GetAsync("/api/v1/settings/platform");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<SettingsReadDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        });
+    }
+
+    [Test]
+    [Order(7)]
+    public async Task UpdateSettings()
+    {
+        var httpContent = TestHelper.CreateJsonObject(new SettingsUpdateDto
+        {
+            RegistrationIsEnabled = true
+        });
+
+        var response = await _httpClient.PutAsync("/api/v1/settings/platform", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<SettingsReadDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        });
+    }
+
+    [Test]
+    [Order(8)]
+    public async Task UpdateSkinsUrl()
+    {
+        var httpContent = TestHelper.CreateJsonObject(new UrlServiceDto(_newTextureUrl));
+
+        var response = await _httpClient.PutAsync("/api/v1/integrations/texture/skins", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        });
+    }
+
+    [Test]
+    [Order(9)]
+    public async Task UpdateCloaksUrl()
+    {
+        var httpContent = TestHelper.CreateJsonObject(new UrlServiceDto(_newTextureUrl));
+
+        var response = await _httpClient.PutAsync("/api/v1/integrations/texture/cloaks", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(10)]
+    public async Task GetSkinsUrl()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/integrations/texture/skins");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model?.Data, Is.Not.Null);
+            Assert.That(model?.Data?.Url, Is.EqualTo(_newTextureUrl));
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(11)]
+    public async Task GetCloaksUrl()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/integrations/texture/cloaks");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model?.Data, Is.Not.Null);
+            Assert.That(model?.Data?.Url, Is.EqualTo(_newTextureUrl));
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(12)]
+    public async Task UpdateSentryDsn()
+    {
+        var httpContent = TestHelper.CreateJsonObject(new UrlServiceDto(_newSenryUrl));
+
+        var response = await _httpClient.PutAsync("/api/v1/integrations/sentry/dsn", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(13)]
+    public async Task GetSentryDsn()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/integrations/sentry/dsn");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<UrlServiceDto>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model?.Data, Is.Not.Null);
+            Assert.That(model?.Data?.Url, Is.EqualTo(_newSenryUrl));
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(14)]
+    public async Task GetLauncherVersions()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/integrations/github/launcher/versions");
+        var content = await response.Content.ReadAsStringAsync();
+
+        var model = JsonConvert.DeserializeObject<ResponseMessage<List<LauncherVersionReadDto>>>(content);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(model, Is.Not.Null);
+            Assert.That(model?.Data, Is.Not.Null);
+            Assert.That(model?.Data?.Any(), Is.True);
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+        });
+    }
+
+    [Test]
+    [Order(15)]
+    public async Task CompileLauncherVersions()
+    {
+        var httpContent = TestHelper.CreateJsonObject(new LauncherCreateDto
+        {
+            GitHubVersions = "master",
+            Host = "https://localhost:5000",
+            Folder = "GmlLauncher"
+        });
+
+        var response = await _httpClient.PostAsync("/api/v1/integrations/github/launcher/download", httpContent);
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.IsSuccessStatusCode, Is.True);
+            Assert.That(string.IsNullOrWhiteSpace(content), Is.False);
+        });
+    }
+
+    [Test]
+    [Order(16)]
+    public async Task DownloadLauncherVersions()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/integrations/github/launcher/download/master");
+        var content = await response.Content.ReadAsStringAsync();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(string.IsNullOrWhiteSpace(content), Is.False);
+            Assert.That(response.IsSuccessStatusCode, Is.True);
         });
     }
 }
