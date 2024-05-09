c3fae098f18b3c9353f976f01b77049f1190ce30
Anton Terentev <gamervii@icloud.com>
2024-05-09 10:17:27 +0300

    Moved projects to src directory
    Refactored the directory structure to move Gml.Web.Api.EndpointSDK and Gml.Web.Api.Plugins.Template under the src directory. This involved adjustments in project references in csproj and sln files as well.

--- Gml.Web.Api.EndpointSDK/IPluginEndpoint.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-
-namespace Gml.Web.Api.EndpointSDK;
-
-public interface IPluginEndpoint
-{
-    Task Execute(HttpContext context);
-}
--- Gml.Web.Api.EndpointSDK/PathAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Gml.Web.Api.EndpointSDK;
-
-public class PathAttribute(string? method, string? path) : Attribute
-{
-    public string? Method { get; set; } = method;
-    public string? Path { get; set; } = path;
-}
