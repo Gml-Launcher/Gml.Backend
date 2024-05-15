59cce65aaffda6ca0b50184fdd4bf3daf8052994
Anton Terentev <gamervii@icloud.com>
2024-05-16 00:23:57 +0300

    Add new AddServerEndpoint and associated csproj file
    This commit introduces a new AddServerEndpoint for server creation along with its associated csproj file. This endpoint uses the FluentValidation library for server input validation.

---
+++ Gml.Web.Api.Plugins.Servers/AddServerEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Gml.Web.Api.EndpointSDK;
+using Gml.Web.Api.Plugins.Servers.Models.DTOs;
+using Gml.Web.Api.Plugins.Servers.Validators;
+using GmlCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Gml.Web.Api.Plugins.Servers;
+
+[Path("POST", "/api/v1/plugins/servers", true)]
+public class AddServerEndpoint : EndpointHelper, IPluginEndpoint
+{
+    public async Task Execute(HttpContext context, IGmlManager gmlManager)
+    {
+        try
+        {
+            var serverDto = await ParseDto<AddServerDto>(context);
+
+            if (!IsValidDto<AddServerDto, ServerCreateValidator>(serverDto, out var errors))
+            {
+                await BadRequest(context, errors.Errors, "Ошибка валидации");
+            }
+
+            var profile = await gmlManager.Profiles.GetProfile(serverDto!.ProfileName);
+
+            if (profile is null)
+            {
+                await NotFound(context, $"Не удалось найти профиль с наименованием: \"{serverDto.ProfileName}\"");
+                return;
+            }
+
+            var server =
+                await gmlManager.Servers.AddMinecraftServer(profile, serverDto.Name, serverDto.Address, serverDto.Port);
+
+            await Created(context, server, "Сервер был успешно создан");
+        }
+        catch (Exception exception)
+        {
+            await BadRequest(context, exception.Message);
+        }
+    }
+}
