400ce15e3aa14c891b4fe5e0d6398c436c474264
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2024-07-09 10:05:25 +0300

    Remove AddServer functionality and related files
    Deleted all files and classes related to the AddServer functionality including DTOs, endpoints, validators, and project references. This change is intended to clean up the codebase and remove unnecessary components.

--- Gml.Web.Api.Plugins.Servers/Models/DTOs/AddServerDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Gml.Web.Api.Plugins.Servers.Models.DTOs;
-
-public class AddServerDto
-{
-    public required string Name { get; set; }
-    public required string Address { get; set; }
-    public required int Port { get; set; }
-    public required string ProfileName { get; set; }
-}
