448c168db8be4121371bbbb2f6c5ef0efec34b39
Akemiko <tcop29711@gmail.com>
2024-10-17 15:50:11 +0300

    Add tests for launcher operations and helper method
    Implemented new unit tests for downloading and building the launcher, and added a helper method to download and extract GitHub projects. These changes enhance test coverage and streamline integration with GitHub repositories.

--- tests/Gml.Backend.Tests/Tests.cs
+++ tests/Gml.Backend.Tests/Tests.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using System.Net;
 using Faker;
+using Gml.Core.Launcher;
+using Gml.Web.Api.Domains.LauncherDto;
 using Gml.Web.Api.Dto.Messages;
 using Gml.Web.Api.Dto.Profile;
 using GmlCore.Interfaces.Enums;
@@ -12,6 +14,12 @@ namespace Gml.Backend.Tests;
 
 public class Tests
 {
+    private GmlManager GmlManager { get; } =
+        new(new GmlSettings("GamerVIILauncher", "gfweagertghuysergfbsuyerbgiuyserg", httpClient: new HttpClient())
+        {
+            TextureServiceEndpoint = "http://gml-web-skins:8085"
+        });
+
     private HttpClient _httpClient;
     private WebApplicationFactory<Program> _webApplicationFactory;
     private readonly string _profileName = "UnitTestProfile";
@@ -20,7 +28,7 @@ public class Tests
     private HubConnection _launcherHub;
 
     [SetUp]
-    public async Task Setup()
+    public void Setup()
     {
         Environment.SetEnvironmentVariable("SECURITY_KEY", "jkuhbsfgvuk4gfikhn8i7wa34rkbqw23");
         Environment.SetEnvironmentVariable("PROJECT_NAME", "GmlServer");
@@ -152,6 +160,101 @@ public class Tests
         _profileHub.On<string>("SuccessPacked", (profileName) =>
         {
             messageReceived.SetResult($"Packaging Profile Success. Profile name: {profileName}");
+        });
+
+        var message = await messageReceived.Task;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(message, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    [Order(5)]
+    public async Task DownloadLauncherTest()
+    {
+        if (_launcherHub.State != HubConnectionState.Connected)
+            await _launcherHub.StartAsync();
+
+        var response = await _httpClient.GetAsync("api/v1/integrations/github/launcher/versions");
+
+        if (response.StatusCode != HttpStatusCode.OK)
+            Assert.Fail();
+
+        var versions = JsonConvert.DeserializeObject<ResponseMessage<List<LauncherVersionReadDto>>>(await response.Content.ReadAsStringAsync());
+
+        var messageReceived = new TaskCompletionSource<string>();
+
+        await _launcherHub.SendCoreAsync("Download", [versions.Data[0].Version, _httpClient.BaseAddress, _profileName]);
+
+        _launcherHub.On("LauncherDownloadEnded", () =>
+        {
+            messageReceived.SetResult("Download Launcher Success.");
+        });
+
+        var message = await messageReceived.Task;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(message, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    [Order(6)]
+    public async Task DownloadLibLauncherTest()
+    {
+        var response = await _httpClient.GetAsync("api/v1/integrations/github/launcher/versions");
+
+        if (response.StatusCode != HttpStatusCode.OK)
+            Assert.Fail();
+
+        var versions = JsonConvert.DeserializeObject<ResponseMessage<List<LauncherVersionReadDto>>>(await response.Content.ReadAsStringAsync());
+
+        var newVersion = versions.Data[0].Version;
+
+        var projectPath = Path.Combine(GmlManager.System.DefaultInstallation, "GmlServer", "Launcher", newVersion, $"Gml.Launcher-{newVersion.Replace("v", "")}", "src");
+
+        // Скачивание Gml.Client
+        var downloadGmlClient = await TestHelper.DownloadGithubProject(projectPath, "Gml-Launcher", "Gml.Client", newVersion);
+
+        // Скачивание GamerVII.Notification.Avalonia
+        var downloadNotificationLib = await TestHelper.DownloadGithubProject(projectPath, "GamerVII-NET", "GamerVII.Notification.Avalonia", "master", false);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(downloadGmlClient, Is.Not.Null);
+            Assert.That(downloadNotificationLib, Is.Not.Null);
+        });
+    }
+
+    [Test]
+    [Order(7)]
+    public async Task BuildLauncherTest()
+    {
+        if (_launcherHub.State != HubConnectionState.Connected)
+            await _launcherHub.StartAsync();
+
+        var response = await _httpClient.GetAsync("api/v1/integrations/github/launcher/versions");
+
+        if (response.StatusCode != HttpStatusCode.OK)
+            Assert.Fail();
+
+        var versions = JsonConvert.DeserializeObject<ResponseMessage<List<LauncherVersionReadDto>>>(await response.Content.ReadAsStringAsync());
+
+        var messageReceived = new TaskCompletionSource<string>();
+
+        await _launcherHub.SendCoreAsync("Compile", [versions.Data[0].Version, new [] { "win-x64" }]);
+
+        _launcherHub.On("LauncherBuildEnded", () =>
+        {
+            messageReceived.SetResult("Compile Launcher Success.");
+        });
+
+        _launcherHub.On<string>("Log", (message) =>
+        {
+            Debug.WriteLine(message);
         });
 
         var message = await messageReceived.Task;
